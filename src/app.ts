7a13557bea20a9fde23290742efafcb98eaa21fc
2D\ianb <ianb@slalom.com>
2018-02-15 15:46:36 -0800

    set up basic routing and views

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 const express = require('express');
+const ejs = require("ejs").__express;
 const app = express();
+app.set("view engine", "ejs");
+app.engine('.ejs', ejs);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/index', function(req, res) {
+    res.render('index');
+});
 
+app.get('/about', function(req, res) {
+    res.render('/pages/about.ejs', {
+        solidGoldVariable: 'solid gold info'
+    });
+});
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
